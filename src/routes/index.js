7daa3f9531aef04996a3c28ff09fdd7e986a1a31
tudemaha <gede_mahardika88@yahoo.co.id>
2023-12-15 18:26:04 +0800

    [feat] update password

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import authRouter from './auth_route.js';
 import foodRouter from './food_route.js';
+import passwordRouter from './password_route.js';
 import sellerAccountRouter from './seller_account_route.js';
 
 const injectApp = (app) => {
   app.use('/auth', authRouter);
   app.use('/foods', foodRouter);
   app.use('/accounts', sellerAccountRouter);
+  app.use('/password', passwordRouter);
   return app;
 };
 
