7856bf85b40747235c4c0e5801ab1d2a8355ff04
tudemaha <gede_mahardika88@yahoo.co.id>
2023-12-15 12:50:28 +0800

    [feat] change seller profile picture

--- src/routes/seller_account_route.js
+++ src/routes/seller_account_route.js
@@ -2,6 +2,10 @@ import express from 'express';
 import multer from 'multer';
 import { fileUploadError, verifyToken } from '../middlewares/index.js';
 import { getStoreDetailHandler, getSellerAccountHandler, updateSellerDetailHandler } from '../controller/index.js';
+import createFilename from '../utils/filename.js';
+import { Merchant } from '../models/index.js';
+import uploadFileToBucket from '../pkg/storage.js';
+import Response from '../dto/responses/default_response.js';
 
 const upload = multer({
   limits: {
@@ -28,6 +32,42 @@ const sellerAccountRouter = express.Router();
 sellerAccountRouter.get('/store', verifyToken(1), getStoreDetailHandler);
 sellerAccountRouter.get('/seller', verifyToken(1), getSellerAccountHandler);
 sellerAccountRouter.put('/seller', verifyToken(1), updateSellerDetailHandler);
+sellerAccountRouter.patch('/seller', verifyToken(1), profileUpload, async (req, res) => {
+  let response;
+  const { decodedToken } = res.locals;
+  const reqFile = req.file;
+
+  if (!reqFile) {
+    response = Response.defaultBadRequest({ error: 'make sure image included' });
+    return res.status(response.code).json(response);
+  }
+
+  const merchant = await Merchant.findOne({
+    where: {
+      AccountId: decodedToken.id,
+    },
+  })
+    .catch((error) => {
+      const err = new Error(error);
+      return err;
+    });
+
+  if (merchant instanceof Error) {
+    response = Response.defaultNotFound(null);
+    return res.status(response.code).json(response);
+  }
+
+  let profileName = merchant.image;
+  if (profileName === null) {
+    profileName = createFilename('profiles/', reqFile.originalname);
+  }
+
+  await merchant.update({ image: profileName });
+  await uploadFileToBucket(process.env.BUCKET_NAME, profileName, reqFile.buffer);
+
+  response = Response.defaultOK('profile picture updated successfully', null);
+  return res.status(response.code).json(response);
+});
 
 sellerAccountRouter.use(fileUploadError);
 
