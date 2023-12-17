a76d80f2925d7b070324454b91f8fded146470c3
Daffa Sulthon Kautsar <sulthondaffakautsar@gmail.com>
2023-12-17 12:34:20 +0700

    init setting

--- src/pkg/orm.js
+++ src/pkg/orm.js
@@ -1,20 +1,20 @@
 import { Sequelize } from 'sequelize';
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASS,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: 'mysql',
-//   },
-// );
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+);
 
-const connectionString = `mysql://${process.env.DB_USER}:`
-  + `${process.env.DB_PASS}@`
-  + `${process.env.DB_HOST}/`
-  + `${process.env.DB_NAME}`
-  + `?socketPath=${process.env.SOCKET_PATH}${process.env.CONNECTION_NAME}`;
-const sequelize = new Sequelize(connectionString);
+// const connectionString = `mysql://${process.env.DB_USER}:`
+//   + `${process.env.DB_PASS}@`
+//   + `${process.env.DB_HOST}/`
+//   + `${process.env.DB_NAME}`
+//   + `?socketPath=${process.env.SOCKET_PATH}${process.env.CONNECTION_NAME}`;
+// const sequelize = new Sequelize(connectionString);
 
 export default sequelize;
