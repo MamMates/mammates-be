c37ebea1110d5951273971aa91549493d8102ede
tudemaha <gede_mahardika88@yahoo.co.id>
2023-12-22 07:42:06 +0800

    add default router and not found middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,4 +1,10 @@
 import { createToken, verifyToken } from './jwt.js';
 import fileUploadError from './file_upload_error.js';
+import notFound from './not_found.js';
 
-export { createToken, verifyToken, fileUploadError };
+export {
+  createToken,
+  verifyToken,
+  fileUploadError,
+  notFound,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,8 @@ import reportRouter from './report_route.js';
 import accountRouter from './account_route.js';
 import mamMatesRouter from './mammates_route.js';
 import orderRouter from './order_route.js';
+import defaultRouter from './default_route.js';
+import { notFound } from '../middlewares/index.js';
 
 const injectApp = (app) => {
   app.use('/auth', authRouter);
@@ -14,6 +16,8 @@ const injectApp = (app) => {
   app.use('/reports', reportRouter);
   app.use('/mam_mates', mamMatesRouter);
   app.use('/orders', orderRouter);
+  app.use('/', defaultRouter);
+  app.use(notFound);
   return app;
 };
 
